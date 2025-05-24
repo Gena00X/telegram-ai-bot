c77a65ef3d66ac45dc3441f7a91ba9d7db7c51f1
Gena00X <gena00x@ya.ru>
2025-05-24 17:34:38 +0300

    Добавлено логирование сообщений в консоли

--- bot/handlers/base.py
+++ bot/handlers/base.py
@@ -1,7 +1,10 @@
 from aiogram import types, Dispatcher
 from aiogram.filters import Command
-
+import logging
+from config import Config
 async def start_handler(message: types.Message):
+    if Config.LOG_MESSAGES=="true":
+        logging.info(f"Получено сообщение от {message.from_user.full_name} (ID: {message.from_user.id}): {message.text}")
     await message.answer(f"Hello, {message.from_user.first_name}!")
 
 def register_handlers(dp: Dispatcher):
--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
-
+import logging
 load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+    LOG_MESSAGES = os.getenv("LOG_MESSAGES", "false")
